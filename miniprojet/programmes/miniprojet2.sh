e055c1e6fa74893547449cfaa59be5bd52eac7a4
lydia-ourdja <lydiaourdja@gmail.com>
2025-11-11 00:03:52 +0100

    mini projet 2: de tsv à html

---
+++ miniprojet/programmes/miniprojet2.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "erreur :il faut faire  $0 <fichier_urls>"
+    exit 1
+fi
+
+fichiertxt=$1
+sortie="../tableaux/tableau-fr.html"
+
+# Début du HTML
+echo "<!DOCTYPE html>" > "$sortie"
+echo "<html lang='fr'>" >> "$sortie"
+echo "<head>" >> "$sortie"
+echo "  <meta charset='UTF-8'>" >> "$sortie"
+echo "  <title>Résultats des URLs</title>" >> "$sortie"
+echo "</head>" >> "$sortie"
+echo "<body>" >> "$sortie"
+echo "<h2>Analyse des URLs</h2>" >> "$sortie"
+echo "<table>" >> "$sortie"
+echo "<tr><th>Num</th><th>URL</th><th>HTTP Code</th><th>Encodage</th><th>Nombre de mots</th></tr>" >> "$sortie"
+
+
+num=0
+while read -r url; do
+
+    num=$((num+1))
+
+    # En-têtes HTTP, nettoyage des \r( pour eviter le décalage dans le fichier tsv )
+    http_info=$(curl -sI "$url" | tr -d '\r')
+
+    # Code HTTP
+    http_code=$(echo "$http_info" | grep -i "HTTP/" | tail -1 | awk '{print $2}')
+    [ -z "$http_code" ] && http_code="inconnu"
+
+    # Charset dans headers
+    encodage=$(echo "$http_info" | grep -i "charset=" | grep -o -E 'charset=([^ ;"]+)' | cut -d= -f2 | head -n1)
+
+    #  charset dans <meta>
+    if [ -z "$encodage" ]; then
+        encodage=$(curl -s "$url" | tr -d '\r' | grep -i "<meta" | grep -i "charset=" \ | grep -o -E 'charset=([^ ;">]+)' | cut -d= -f2 | head -n1)
+    fi
+    #rendre inconnu si il trouve nulpart l'encodage
+    [ -z "$encodage" ] && encodage="inconnu"
+
+    # Nombre de mots ( sed ici permet d'enlever toutes les balises HTML et de pas les comptabiliser dans le nombre de mots  )
+    nb_mots=$(curl -s "$url" | sed 's/<[^>]*>//g' | wc -w)
+
+    # Affichage
+
+printf "<tr><td>%s</td><td><a href='%s' target='_blank'>%s</a></td><td>%s</td><td>%s</td><td>%s</td></tr>\n" \
+        "$num" "$url" "$url" "$http_code" "$encodage" "$nb_mots" >> "$sortie"
+
+
+done < "$fichiertxt"
+
+# Fin du HTML
+
+echo "</table>" >> "$sortie"
+echo "<p>Résultats générés le $(date)</p>" >> "$sortie"
+echo "</body>" >> "$sortie"
+echo "</html>" >> "$sortie"
+
+echo "Résultats enregistrés dans $sortie"
